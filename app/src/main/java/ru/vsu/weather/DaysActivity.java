ce8dc42623f598c50050b73e1a3293cddd0961e3
Lilia_Tsaplina <foxtrot7777@mail.ru>
2018-07-08 23:02:40 +0300

    Initial commit

---
+++ app/src/main/java/ru/vsu/weather/DaysActivity.java
@@ -0,0 +1,13 @@
+package ru.vsu.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaysActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_days);
+    }
+}
